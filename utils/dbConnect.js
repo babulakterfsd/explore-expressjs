6b3a1e1ab6f078896e18892ca85ff1f1a48a5dc6
Babul Akter <babulakterfsd@gmail.com>
2023-04-27 06:19:05 +0600

    linting setup added with prettier.

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,15 +1,15 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require('mongodb');
 
- async function connectDB() {
+async function connectDB() {
     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ooo4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
     const client = new MongoClient(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     });
 
     await client.connect();
-    const db = client.db("tourguru");
+    const db = client.db('tourguru');
     return db;
 }
 
