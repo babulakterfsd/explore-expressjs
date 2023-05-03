f63924577eb2c452a192392977f8262d3cd84272
Babul Akter <babulakterfsd@gmail.com>
2023-05-03 11:14:09 +0600

    error handled with try catch in controllers

--- controllers/packages.controller.js
+++ controllers/packages.controller.js
@@ -2,52 +2,68 @@ const { ObjectId } = require('mongodb');
 const connectDB = require('../utils/dbConnect');
 
 module.exports.getAllPackages = async (req, res) => {
-    const database = await connectDB();
-    const packageCollection = database.collection('tourpackages');
-    if (req.query.location) {
-        const { location } = req.query;
-        const filter = { 'location.city': { $regex: location, $options: 'i' } };
-        const result = await packageCollection.find(filter).toArray();
-        if (result.length === 0) {
-            // return res
-            //   .status(404)
-            //   .json({ status: "false", message: "No Package Found" });
-            return res.json([]);
+    try {
+        const database = await connectDB();
+        const packageCollection = database.collection('tourpackages');
+        if (req.query.location) {
+            const { location } = req.query;
+            const filter = { 'location.city': { $regex: location, $options: 'i' } };
+            const result = await packageCollection.find(filter).toArray();
+            if (result.length === 0) {
+                // return res
+                //   .status(404)
+                //   .json({ status: "false", message: "No Package Found" });
+                return res.json([]);
+            }
+            return res.status(200).json(result);
         }
-        return res.status(200).json(result);
+        const limit = +req.query.limit;
+        let result;
+        if (limit) {
+            result = await packageCollection.find({}).sort({ _id: -1 }).limit(limit).toArray();
+        } else {
+            result = await packageCollection.find({}).sort({ _id: -1 }).toArray();
+        }
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' });
     }
-    const limit = +req.query.limit;
-    let result;
-    if (limit) {
-        result = await packageCollection.find({}).sort({ _id: -1 }).limit(limit).toArray();
-    } else {
-        result = await packageCollection.find({}).sort({ _id: -1 }).toArray();
-    }
-    res.json(result);
 };
 
 module.exports.addANewPackage = async (req, res) => {
-    const database = await connectDB();
-    const packageCollection = database.collection('tourpackages');
-    const newPackage = req.body;
-    const result = await packageCollection.insertOne(newPackage);
-    res.json(newPackage);
+    try {
+        const database = await connectDB();
+        const packageCollection = database.collection('tourpackages');
+        const newPackage = req.body;
+        const result = await packageCollection.insertOne(newPackage);
+        res.json(newPackage);
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' });
+    }
 };
 
 module.exports.getASinglePackage = async (req, res) => {
-    const database = await connectDB();
-    const packageCollection = database.collection('tourpackages');
-    const { packageid } = req.params;
-    const query = { _id: ObjectId(packageid) };
-    const pack = await packageCollection.findOne(query);
-    res.send(pack);
+    try {
+        const database = await connectDB();
+        const packageCollection = database.collection('tourpackages');
+        const { packageid } = req.params;
+        const query = { _id: ObjectId(packageid) };
+        const pack = await packageCollection.findOne(query);
+        res.send(pack);
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' });
+    }
 };
 
 module.exports.deleteAPackage = async (req, res) => {
-    const database = await connectDB();
-    const packageCollection = database.collection('tourpackages');
-    const id = req.params.packageid;
-    const query = { _id: ObjectId(id) };
-    const result = await packageCollection.deleteOne(query);
-    res.json(result);
+    try {
+        const database = await connectDB();
+        const packageCollection = database.collection('tourpackages');
+        const id = req.params.packageid;
+        const query = { _id: ObjectId(id) };
+        const result = await packageCollection.deleteOne(query);
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' });
+    }
 };
