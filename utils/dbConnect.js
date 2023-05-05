37884b4257c0c150dd1bbc342b8f5418b7ac50d4
Babul Akter <babulakterfsd@gmail.com>
2023-05-05 09:38:33 +0600

    database connection optimized

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,6 +1,10 @@
 const { MongoClient } = require('mongodb');
 
+let db;
+
 async function connectDB() {
+    if (db) return db;
+
     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ooo4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
     const client = new MongoClient(uri, {
@@ -9,7 +13,7 @@ async function connectDB() {
     });
 
     await client.connect();
-    const db = client.db('tourguru');
+    db = client.db('tourguru');
     return db;
 }
 
